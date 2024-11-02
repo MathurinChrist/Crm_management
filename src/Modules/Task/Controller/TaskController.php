532e0e74ab496606ca4aade9a3a93cbe3172e2db
Bobann <Kimbembebobann2001@gmail.com>
2024-11-02 18:45:16 +0100

    feat: add route for task creation

--- src/Modules/Task/Controller/TaskController.php
+++ src/Modules/Task/Controller/TaskController.php
@@ -2,28 +2,54 @@
 
 namespace App\Modules\Task\Controller;
 
+use App\Modules\Task\Entity\Task;
 use App\Modules\Task\Services\TaskService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
+#[Route('/task', name: 'task_')]
 class TaskController extends AbstractController
 {
 
     public function  __construct(
-        private readonly TaskService $taskService
+        private readonly TaskService $taskService,
+        private readonly SerializerInterface $serializer,
+        private readonly ValidatorInterface $validator
     ){}
 
     #[Route('/allTask', name: 'task_all',methods: ["GET"])]
     public function getAllTask(): Response
     {
+        //todo! we've to come back for roles
         $allTask = $this->taskService->getAllTask();
+        $this->serializer->serialize($allTask, 'json');
+
         return $this->json([
             'result' => true,
             'total' => count($allTask),
             'data' => $allTask,
             'error' => []
-        ], );
+        ]);
     }
 
+    #[Route('/create', name: 'task_create',methods: ["POST"])]
+    public function createTask(Request $request): Response
+    {
+        $data = json_decode($request->getContent(), true);
+        $result = true;
+        $task = $this->serializer->deserialize($request->getContent(),Task::class, 'json');
+        $errors = $this->validator->validate($task);
+        $this->taskService->createTask($task);
+        //todo: add validator and conditio, for errors if exist
+        return $this->json([
+            'result' => $result,
+            'data' => $task,
+            'error' => []
+        ]);
+
+    }
 }
