a06131921527c820903182ae22928d7784202045
Bobann <Kimbembebobann2001@gmail.com>
2024-11-24 16:16:03 +0100

    fix: add description in migration, refactor file and drop the old database for preparation relation on task and project entity

--- migrations/Version20241027222240.php
+++ migrations/Version20241027222240.php
@@ -14,18 +14,16 @@ final class Version20241027222240 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'migration for entity task creation';
     }
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE task (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, context VARCHAR(255) NOT NULL, project_to VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, status VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('DROP TABLE task');
     }
 }
