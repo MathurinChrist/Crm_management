12396c97ee38e8439caf30f46534eb9e08eb2fdc
Bobann <Kimbembebobann2001@gmail.com>
2025-03-16 12:17:19 +0100

    feat: authentication jwt and user entity creation

---
+++ src/Security/Repository/UserRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Security\Repository;
+
+use App\Security\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserRepository extends ServiceEntityRepository implements UserLoaderInterface
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    //    /**
+    //     * @return Project
+    //[] Returns an array of Project
+    // objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('t.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Project
+
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+    public function loadUserByIdentifier(string $identifier): ?UserInterface
+    {
+        // TODO: Implement loadUserByIdentifier() method.
+        return null;
+    }
+}
