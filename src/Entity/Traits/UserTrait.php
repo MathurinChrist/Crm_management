9cd988a7fc3781834562f117d6bbb984fde05df5
mathurin Kimbembe <mathurin.kimbembe@k-tns.com>
2025-07-19 15:15:42 +0200

    fix: limit deep for avoiding circular error

--- src/Entity/Traits/UserTrait.php
+++ src/Entity/Traits/UserTrait.php
@@ -5,17 +5,20 @@ namespace App\Entity\Traits;
 use App\Security\Entity\User;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Attribute\Groups;
+use Symfony\Component\Serializer\Attribute\MaxDepth;
 
 trait UserTrait
 {
     #[ORM\ManyToOne(targetEntity: User::class)]
     #[ORM\JoinColumn(name: "createdBy", referencedColumnName: "id", nullable: true)]
     #[Groups(['project:read', 'user:read'])]
+    #[MaxDepth(1)]
     private ?User $createdBy;
 
     #[ORM\ManyToOne(targetEntity: User::class)]
     #[ORM\JoinColumn(name: "updatedBy", referencedColumnName: "id", nullable: true)]
     #[Groups(['project:read', 'user:read'])]
+    #[MaxDepth(1)]
     private ?User $updatedBy;
 
     public function getCreatedBy(): ?User
